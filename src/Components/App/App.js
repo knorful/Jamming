d9fadd5753fea8fbbd81638b7612bfde8659c1e2
Kris Norful <krisnorful@gmail.com>
2021-03-18 22:22:46 -0500

    add navbar and search

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component --> */}
+            {/* <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
