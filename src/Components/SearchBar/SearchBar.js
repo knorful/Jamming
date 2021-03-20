9a1022ccd411b70457aea274ba6febe99b696fb6
Kris Norful <krisnorful@gmail.com>
2021-03-20 15:26:52 -0500

    add new background image

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,11 +3,12 @@ import "./SearchBar.css";
 
 class SearchBar extends Component {
   state = {
-    term: "",
+    term: sessionStorage.getItem("searchTerm") || "",
   };
 
   search = () => {
     const searchTerm = this.state.term;
+    sessionStorage.setItem("searchTerm", searchTerm);
     this.props.onSearch(searchTerm);
     this.props.click();
   };
@@ -24,6 +25,7 @@ class SearchBar extends Component {
         <input
           onChange={this.handleTermChange}
           placeholder="Enter A Song, Album, or Artist"
+          value={this.state.term}
         />
         <button onClick={this.search} className="SearchButton">
           SEARCH
