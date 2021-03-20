9442bd8ec4ff4c1aee78b433ffcc844ed02c60c2
Kris Norful <krisnorful@gmail.com>
2021-03-20 14:00:44 -0500

    add save message once saved button is clicked

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import TrackList from "../TrackList/TrackList";
+import Loader from "react-loader-spinner";
 import "./Playlist.css";
 
 class Playlist extends Component {
+  state = {
+    saving: false,
+    message: "",
+  };
+
   handleNameChange = (e) => {
     const newName = e.target.value;
     this.props.onNameChange(newName);
+  };
+
+  handleSaveClick = () => {
+    this.setState({
+      saving: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        saving: false,
+        message: "Playlist Saved",
+      });
+    }, 2000);
   };
 
   render() {
@@ -17,9 +35,24 @@ class Playlist extends Component {
           isRemoval={true}
           tracks={this.props.playlistTracks}
         />
-        <button onClick={this.props.onSave} className="Playlist-save">
-          SAVE TO SPOTIFY
-        </button>
+        {!this.state.saving ? (
+          <>
+            <button
+              onClick={() => {
+                this.props.onSave();
+                this.handleSaveClick();
+              }}
+              className="Playlist-save"
+            >
+              SAVE TO SPOTIFY
+            </button>
+            <span className="playlist-msg">{this.state.message}</span>
+          </>
+        ) : (
+          <div className="loader">
+            <Loader type="Audio" color="#89b198" height={80} width={80} />
+          </div>
+        )}
       </div>
     );
   }
