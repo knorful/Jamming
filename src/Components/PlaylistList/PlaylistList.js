b5cdef98f6a4a63bc88bedc13b6d93dc8b4b3066
Kris Norful <krisnorful@gmail.com>
2021-03-22 19:48:46 -0500

    retrieve user playlist from Spotify on mount of component

--- src/Components/PlaylistList/PlaylistList.js
+++ src/Components/PlaylistList/PlaylistList.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import { Spotify } from "../../util/Spotify";
 
 class PlaylistList extends Component {
+  state = {
+    userPlaylists: [],
+  };
+
+  componentDidMount() {
+    Spotify.getUserPlaylists().then((res) => console.log(res));
+  }
   render() {
     return <div>PlaylistList</div>;
   }
