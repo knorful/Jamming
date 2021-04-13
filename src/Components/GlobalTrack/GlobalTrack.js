f0e74d73f9d7e5c7273c562b02bf44082a2bfc44
Kris Norful <krisnorful@gmail.com>
2021-04-13 16:31:45 -0500

    create GlobalTrack component

---
+++ src/Components/GlobalTrack/GlobalTrack.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import ReactAudioPlayer from "react-audio-player";
+import "./GlobalTrack.css";
+
+class GlobalTrack extends Component {
+  state = {
+    show: false,
+  };
+  addTrack = () => {
+    this.props.onAdd(this.props.track);
+  };
+  removeTrack = () => {
+    this.props.onRemove(this.props.track);
+  };
+
+  show = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+  render() {
+    const add = (
+      <button onClick={this.addTrack} className="Track-action">
+        +
+      </button>
+    );
+    const remove = (
+      <button onClick={this.removeTrack} className="Track-action">
+        -
+      </button>
+    );
+
+    // let hasPreview = this.props.track.preview_url ? true : false;
+    let props = this.props;
+    return (
+      <div className="GlobalTrack">
+        <div className="GlobalTrack-information">
+          <p className="GlobalTrack-rank">{props.rank}</p>
+          <div style={{ color: "black" }}>
+            <h3>{props.track.name}</h3>
+            <p>
+              {props.track.artist} | {props.track.album}
+            </p>
+            {/* {hasPreview ? (
+              <ReactAudioPlayer src={this.props.track.preview_url} controls />
+            ) : null} */}
+          </div>
+        </div>
+
+        {props.remove ? remove : add}
+      </div>
+    );
+  }
+}
+
+export default GlobalTrack;
