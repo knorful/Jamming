9e5deabfd74bf0b1364e82881ab260762e7709f5
Kris Norful <krisnorful@gmail.com>
2021-04-14 06:40:45 -0500

    fix error where saving loader was not showing upon save

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -10,7 +10,12 @@ class Playlist extends Component {
     saving: false,
     message: "",
     userPlaylists: [],
+    loggedIn: sessionStorage.getItem("loggedIn") || false,
   };
+
+  componentDidMount() {
+    sessionStorage.clear();
+  }
 
   handleNameChange = (e) => {
     const newName = e.target.value;
@@ -18,10 +23,9 @@ class Playlist extends Component {
   };
 
   handleSaveClick = () => {
-    this.setState((st) => ({
+    this.setState({
       saving: true,
-      ...st,
-    }));
+    });
     setTimeout(() => {
       this.setState({
         saving: false,
@@ -40,44 +44,60 @@ class Playlist extends Component {
     });
   }
 
+  logIn() {
+    sessionStorage.setItem("loggedIn", "true");
+    this.getPlaylists();
+  }
+
   render() {
     console.log("rendering...");
     return (
       <div className="Playlist">
-        <input
-          onChange={this.handleNameChange}
-          placeholder="Enter Playlist Name"
-          value={this.props.playlistName}
-        />
-        <TrackList
-          onRemove={this.props.onRemove}
-          isRemoval={true}
-          tracks={this.props.playlistTracks}
-        />
-        {!this.state.saving ? (
-          <>
-            <button
-              onClick={async () => {
-                await this.getPlaylists();
-                this.handleSaveClick();
-                this.props.onSave();
-                this.getPlaylists();
-              }}
-              className="Playlist-save"
-            >
-              SAVE TO SPOTIFY
+        {!this.state.loggedIn ? (
+          <div>
+            <p>Log in to begin creating your custom playlist</p>
+            <button onClick={() => this.logIn()} className="Playlist-save">
+              LOGIN TO SPOTIFY
             </button>
-            <span className="playlist-msg">{this.state.message}</span>
-            <PlaylistList
-              userPlaylists={this.state.userPlaylists}
-              selectPlaylist={this.props.selectPlaylist}
-            />
-          </>
-        ) : (
-          <div className="loader">
-            <p className="loader-msg">Saving...</p>
-            <Loader type="Audio" color="#89b198" height={80} width={80} />
           </div>
+        ) : (
+          <>
+            <input
+              onChange={this.handleNameChange}
+              placeholder="Enter Playlist Name"
+              value={this.props.playlistName}
+            />
+            <TrackList
+              onRemove={this.props.onRemove}
+              isRemoval={true}
+              tracks={this.props.playlistTracks}
+            />
+            {!this.state.saving ? (
+              <>
+                <button
+                  onClick={async () => {
+                    await this.getPlaylists();
+                    this.handleSaveClick();
+                    this.props.onSave();
+                    await this.getPlaylists();
+                  }}
+                  className="Playlist-save"
+                >
+                  SAVE TO SPOTIFY
+                </button>
+                <span className="playlist-msg">{this.state.message}</span>
+                <PlaylistList
+                  userPlaylists={this.state.userPlaylists}
+                  selectPlaylist={this.props.selectPlaylist}
+                />
+              </>
+            ) : (
+              <div className="loader">
+                <p className="loader-msg">Saving...</p>
+                <Loader type="Audio" color="#89b198" height={80} width={80} />
+              </div>
+            )}
+          </>
         )}
       </div>
     );
